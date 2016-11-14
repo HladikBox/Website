35e5e90b761fc4f6c550bcaa3a49be4c231eb479
steve.cai <alucard263096@126.com>
2016-11-14 23:55:36 +0800

    上传初始代码

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Created on 2012-6-30
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+  require 'include/common.inc.php';
+  include ROOT.'/include/init.inc.php';
+  
+//  include ROOT.'/classes/mgr/wechat.cls.php';
+//  $signPackage = $WechatMgr->GetSignPackage();
+//  $userInfo = $WechatMgr->getUserBaseInfo();
+
+//  $smarty->assign("wechatsign",$signPackage);
+//  $smarty->assign("userinfo",$userInfo);
+  
+  $smarty->display(ROOT.'/templates/index.html');
+  
+?>
